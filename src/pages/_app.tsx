33eaf9cc2fdb21094e9b467c3afb68accc462290
jangyonghan <wkddydgks1@gamil.com>
2024-10-02 13:54:49 +0900

    스타일드 컴포넌트 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </>
+  );
 }
